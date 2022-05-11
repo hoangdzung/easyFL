4e97d02e9b0e9573d5fad93871c05e35da677562
hoangdzung <hoangdzung@github.com>
2022-05-11 15:18:29 +0200

    aggregate by mask

--- algorithm/mp_fedavg_dist8.py
+++ algorithm/mp_fedavg_dist8.py
@@ -19,7 +19,7 @@ class Server(MPBasicServer):
 
         X = []
         for i, (client, model) in enumerate(zip(self.selected_clients, models)):
-            labels_to_clients[tuple(sorted(client[i].all_labels))].append(i)
+            labels_to_clients[tuple(sorted(self.clients[client].all_labels))].append(i)
             X.append(model.fc1.weight.data.flatten().detach().cpu().numpy())
         
         y = np.zeros(len(X))
@@ -29,7 +29,40 @@ class Server(MPBasicServer):
         kmeans = KMeans(n_clusters=self.num_groups, random_state=self.seed).fit(X)   
         y_pred = kmeans.labels_
         print(metrics.homogeneity_score(y, y_pred), metrics.completeness_score(y, y_pred))
-    
+
+    def aggregate(self, models):
+        """
+        Returns the average of the weights.
+        """
+        state_dicts = [model.state_dict() for model in models]
+        K = models[0].fc2.weight.data.shape[0]
+
+        masks = []
+        for client in self.selected_clients:
+            mask = np.zeros((K,1))
+            mask[self.clients[client].all_labels] = 1
+            masks.append(mask)
+
+        for i, (client, model) in enumerate(zip(self.selected_clients, models)):
+            labels_to_clients[tuple(sorted(self.clients[client].all_labels))].append(i)
+
+        avg_state_dict = copy.deepcopy(state_dicts[0])
+        for key in avg_state_dict.keys():
+            if not key.startswith('fc2'):
+                for i in range(1, len(state_dicts)):
+                    avg_state_dict[key] += state_dicts[i][key]
+                avg_state_dict[key] = torch.div(avg_state_dict[key], len(state_dicts))
+            else:
+                avg_state_dict[key] = avg_state_dict[key] * masks[0]
+                mask = mask[0]
+                for i in range(1, len(state_dicts)):
+                    avg_state_dict[key] += avg_state_dict[i][key]*masks[i]
+                    mask += masks[i]
+
+                avg_state_dict[key] = avg_state_dict[key]/mask
+
+        self.model.load_state_dict(avg_state_dict)
+
     def iterate(self, t, pool):
         """
         The standard iteration of each federated round that contains three
@@ -46,7 +79,8 @@ class Server(MPBasicServer):
         # aggregate: pk = 1/K as default where K=len(selected_clients)
         device0 = torch.device(f"cuda:{self.server_gpu_id}")
         models = [i.to(device0) for i in models]
-        self.model = self.aggregate(models, p = [1.0 for cid in self.selected_clients])
+        # self.model = self.aggregate(models, p = [1.0 for cid in self.selected_clients])
+        self.aggregate(models)
         self.compare_model(models)
         return
 
@@ -55,11 +89,6 @@ class Client(MPBasicClient):
     def __init__(self, option, name='', train_data=None, valid_data=None):
         super(Client, self).__init__(option, name, train_data, valid_data)
         self.all_labels = self.train_data.all_labels if self.train_data else [] 
-
-    def mask_weight(self, model):
-        
-        missing_labels = [i for i in range(model.fc2.weight.shape[0]) if i not in self.all_labels ]
-        model.fc2.weight.data[missing_labels,:] = 0
                 
     def reply(self, svr_pkg, device):
         """
@@ -77,7 +106,6 @@ class Client(MPBasicClient):
         model = self.unpack(svr_pkg)
         loss = self.train_loss(model, device)
         self.train(model, device)
-        self.mask_weight(model)
         cpkg = self.pack(model, loss)
         return cpkg
 
