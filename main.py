1bffcf471d5a17e4c22882ec89babd23d405879f
hoangdzung <hoangdzung@github.com>
2022-05-25 14:25:14 +0200

    minor fix print

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class MyLogger(flw.Logger):
         
         valid_metrics, valid_losses = server.test_on_clients(self.current_round, 'valid')
         train_metrics, train_losses = server.test_on_clients(self.current_round, 'train')
-        is_branchy = type(train_metrics[0]) == tuple
+        is_branchy = type(test_metric[0]) == tuple
         if not is_branchy:
             self.output['train_losses'].append(1.0*sum([ck * closs for ck, closs in zip(server.client_vols, train_losses)])/server.data_vol)
             self.output['mean_valid_accs'].append(1.0*sum([ck * acc for ck, acc in zip(server.client_vols, valid_metrics)])/server.data_vol)
@@ -41,16 +41,19 @@ class MyLogger(flw.Logger):
                 self.output['client_accs'][server.clients[cid].name]=[self.output['valid_accs'][i][cid] for i in range(len(self.output['valid_accs']))]
     
         if not is_branchy:
-            print(self.temp.format("Training Loss:", self.output['train_losses'][-1]))
-        print(self.temp.format("Testing Loss:", self.output['test_losses'][-1]))
-        print(self.temp.format("Testing Accuracy:", self.output['test_accs'][-1]))
-    
-        if not is_branchy:
+            print(self.temp.format("Training Loss:", self.output['train_losses'][-1]))     
+            print(self.temp.format("Testing Loss:", self.output['test_losses'][-1]))
+            print(self.temp.format("Testing Accuracy:", self.output['test_accs'][-1]))   
             print(self.temp.format("Validating Accuracy:", self.output['mean_valid_accs'][-1]))
-        print(self.temp.format("Mean of Client Accuracy:", self.output['mean_curve'][-1]))
-        print(self.temp.format("Std of Client Accuracy:", self.output['var_curve'][-1]))
+            print(self.temp.format("Mean of Client Accuracy:", self.output['mean_curve'][-1]))
+            print(self.temp.format("Std of Client Accuracy:", self.output['var_curve'][-1]))
 
-
+        else:
+            print(self.temp.format("Training Loss:", self.output['train_losses'][-1]))
+            print("Testing Loss:", self.output['test_losses'][-1])
+            print("Testing Accuracy:", self.output['test_accs'][-1])
+            print(self.temp.format("Mean of Client Accuracy:", self.output['mean_curve'][-1]))
+            print(self.temp.format("Std of Client Accuracy:", self.output['var_curve'][-1]))
 logger = MyLogger()
 
 def main():
