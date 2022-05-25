9d3405721ec8896433b5d77a3e57b20f16be9ee3
hoangdzung <hoangdzung@github.com>
2022-05-25 14:27:13 +0200

    minor fix print

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ class MyLogger(flw.Logger):
             test_metric, test_loss = server.test(device=torch.device('cuda:0'))
         else:
             test_metric, test_loss = server.test()
+        is_branchy = type(test_metric) not in [list, tuple]
         
         valid_metrics, valid_losses = server.test_on_clients(self.current_round, 'valid')
         train_metrics, train_losses = server.test_on_clients(self.current_round, 'train')
-        is_branchy = type(test_metric[0]) == tuple
         if not is_branchy:
             self.output['train_losses'].append(1.0*sum([ck * closs for ck, closs in zip(server.client_vols, train_losses)])/server.data_vol)
             self.output['mean_valid_accs'].append(1.0*sum([ck * acc for ck, acc in zip(server.client_vols, valid_metrics)])/server.data_vol)
