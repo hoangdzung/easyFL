7e9fe12872b73a906525fa6563362ab65c8e9f5e
hoangdzung <hoangdzung@github.com>
2022-05-29 16:06:43 +0200

    add dropout

--- benchmark/mnist/model/branchy.py
+++ benchmark/mnist/model/branchy.py
@@ -6,12 +6,12 @@ from utils.fmodule import FModule
 class Model(FModule):
     def __init__(self):
         super().__init__()
-        # self.base = nn.Sequential(
-        #     nn.Conv2d(in_channels=1, out_channels=5, kernel_size=5, stride=1, padding=3)
-        # ) 
+        self.base = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=5, kernel_size=5, stride=1, padding=3)
+        ) 
         self.flatten = nn.Flatten()
         self.branch1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=5, kernel_size=5, stride=1, padding=3)
+            # nn.Conv2d(in_channels=1, out_channels=5, kernel_size=5, stride=1, padding=3)
             nn.MaxPool2d(2, 2),
             nn.ReLU(), 
             nn.Conv2d(in_channels=5, out_channels=5, kernel_size=3, stride=3, padding=1),
@@ -22,17 +22,21 @@ class Model(FModule):
         self.fc1 = nn.Linear(20,10)
             
         self.branch2 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=5, kernel_size=5, stride=1, padding=3)
+            # nn.Conv2d(in_channels=1, out_channels=5, kernel_size=5, stride=1, padding=3)
             nn.MaxPool2d(2, 2),            
             nn.ReLU(),
+            nn.Dropout2d(0.5)
             nn.Conv2d(in_channels=5, out_channels=10, kernel_size=5, stride=1, padding=3),
             nn.MaxPool2d(2, 2), 
             nn.ReLU(),
+            nn.Dropout2d(0.5)
             nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5, stride=1, padding=3),
             nn.MaxPool2d(2, 2), 
             nn.ReLU(),
+            nn.Dropout2d(0.5)
             nn.Flatten(),
             nn.Linear(500,84),
+            nn.Dropout1d(0.5)
         )
         self.fc2 = nn.Sequential(
             nn.ReLU(),
@@ -41,7 +45,7 @@ class Model(FModule):
         self.exit_threshold = 0.3
         
     def forward(self, x, n=0):
-        # x_base = self.base(x)
+        x = self.base(x)
         if n ==0:
             x = self.branch1(x)
             x = self.fc1(x)
@@ -58,12 +62,12 @@ class Model(FModule):
     
     def pred_and_rep(self, x, n):
         if n ==0:
-            # x = self.base(x)
+            x = self.base(x)
             e = self.branch1(x)
             o = self.fc1(e)
             return o, [e]
         else:
-            # x = self.base(x)
+            x = self.base(x)
             e = self.branch2(x)
             o = self.fc2(e) 
             return o, [e]
