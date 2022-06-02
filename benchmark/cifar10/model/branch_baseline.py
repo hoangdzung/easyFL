b51c714e119958c888aeac93c204c89fa4a7a3f6
hoangdzung <hoangdzung@github.com>
2022-06-02 11:43:50 +0200

    mnist-based models

--- benchmark/cifar10/model/branch_baseline.py
+++ benchmark/cifar10/model/branch_baseline.py
@@ -7,30 +7,37 @@ class Model(FModule):
     def __init__(self):
         super().__init__()
         self.base_layer0 = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=0),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=0),
+            nn.Conv2d(3, 5, kernel_size=5, stride=1, padding=3),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(5),
             nn.ReLU()
         )
 
         self.base_layer1 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=0),
+            nn.Conv2d(5, 10, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(10),
+            nn.ReLU()
+        )
+        self.base_layer2 = nn.Sequential(
+            nn.Conv2d(10, 20, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(20),
             nn.ReLU()
         )
 
         self.base_gap = torch.nn.AdaptiveAvgPool2d(1)
         self.base_flatten = nn.Flatten()
-        self.base_fc = torch.nn.Linear(64, 10)
-
-    def forward(self, x, n=0):
+        self.base_fc = torch.nn.Linear(20, 10)
+        
+    def forward(self, x):
         x = self.base_layer0(x)
         x = self.base_layer1(x)
+        x = self.base_layer2(x)
         x = self.base_gap(x)
         x = self.base_flatten(x)
         x = self.base_fc(x)
-        
         return x
-        
+
+
 class Loss(nn.Module):
     def __init__(self):
         super(Loss, self).__init__()
--- benchmark/cifar10/model/branchy.py
+++ benchmark/cifar10/model/branchy.py
@@ -1,157 +1,75 @@
-"""resnet in pytorch
-
-
-
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun.
-
-    Deep Residual Learning for Image Recognition
-    https://arxiv.org/abs/1512.03385v1
-"""
-import torch.nn as nn
+import torch
+from torch import nn
+import torch.nn.functional as F
 from utils.fmodule import FModule
 
-class BasicBlock(nn.Module):
-    """Basic Block for resnet 18 and resnet 34
-
-    """
-
-    #BasicBlock and BottleNeck block
-    #have different output size
-    #we use class attribute expansion
-    #to distinct
-    expansion = 1
-
-    def __init__(self, in_channels, out_channels, stride=1):
-        super().__init__()
-
-        #residual function
-        self.residual_function = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels * BasicBlock.expansion)
-        )
-
-        #shortcut
-        self.shortcut = nn.Sequential()
-
-        #the shortcut output dimension is not the same with residual function
-        #use 1*1 convolution to match the dimension
-        if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels * BasicBlock.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels * BasicBlock.expansion)
-            )
-
-    def forward(self, x):
-        return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
-
-class BottleNeck(nn.Module):
-    """Residual block for resnet over 50 layers
-
-    """
-    expansion = 4
-    def __init__(self, in_channels, out_channels, stride=1):
-        super().__init__()
-        self.residual_function = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, stride=stride, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels * BottleNeck.expansion, kernel_size=1, bias=False),
-            nn.BatchNorm2d(out_channels * BottleNeck.expansion),
-        )
-
-        self.shortcut = nn.Sequential()
-
-        if stride != 1 or in_channels != out_channels * BottleNeck.expansion:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels * BottleNeck.expansion, stride=stride, kernel_size=1, bias=False),
-                nn.BatchNorm2d(out_channels * BottleNeck.expansion)
-            )
-
-    def forward(self, x):
-        return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
-
 class Model(FModule):
-    def __init__(self, block=BasicBlock, num_block=[2,2,2,2], num_classes=10):
+    def __init__(self):
         super().__init__()
-        self.in_channels = 64
-        self.base_conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True))
-        #we use a different inputsize than the original paper
-        #so conv2_x's stride is 1
-        self.base_conv2_x = self._make_layer(block, 64, num_block[0], 1)
-        self.base_conv3_x = self._make_layer(block, 128, num_block[1], 2)
-        self.branch2_conv4_x = self._make_layer(block, 256, num_block[2], 2)
-        self.branch2_conv5_x = self._make_layer(block, 512, num_block[3], 2)
-        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.branch1_fc = nn.Linear(128 * block.expansion, num_classes)
-        self.branch2_fc = nn.Linear(512 * block.expansion, num_classes)
+        self.base_layer0 = nn.Sequential(
+            nn.Conv2d(3, 5, kernel_size=5, stride=1, padding=3),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(5),
+            nn.ReLU()
+        )
 
-    def _make_layer(self, block, out_channels, num_blocks, stride):
-        """make resnet layers(by layer i didnt mean this 'layer' was the
-        same as a neuron netowork layer, ex. conv layer), one layer may
-        contain more than one residual block
+        self.base_layer1 = nn.Sequential(
+            nn.Conv2d(5, 10, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(10),
+            nn.ReLU()
+        )
 
-        Args:
-            block: block type, basic block or bottle neck block
-            out_channels: output depth channel number of this layer
-            num_blocks: how many blocks per layer
-            stride: the stride of the first block of this layer
+        self.base_layer2 = nn.Sequential(
+            nn.Conv2d(10, 20, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(20),
+            nn.ReLU()
+        )
 
-        Return:
-            return a resnet layer
-        """
+        self.base_layer3 = nn.Sequential(
+            nn.Conv2d(20, 40, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(40),
+            nn.ReLU()
+        )
+        
+        self.base_gap = torch.nn.AdaptiveAvgPool2d(1)
+        self.base_flatten = nn.Flatten()
+        self.branch1_fc = torch.nn.Linear(20, 10)
+        self.branch2_fc = torch.nn.Linear(40, 10)
 
-        # we have num_block blocks per layer, the first block
-        # could be 1 or 2, other blocks would always be 1
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_channels, out_channels, stride))
-            self.in_channels = out_channels * block.expansion
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x,n=0):
-        output = self.base_conv1(x)
-        output = self.base_conv2_x(output)
-        output = self.base_conv3_x(output)
-        if n ==0:
-            output = self.avg_pool(output)
-            output = output.view(output.size(0), -1)
-            output = self.branch1_fc(output)
+    def forward(self, x, n=0):
+        x = self.base_layer0(x)
+        x = self.base_layer1(x)
+        x = self.base_layer2(x)
+    
+        if n==0:
+            x = self.base_gap(x)
+            x = self.base_flatten(x)
+            x = self.branch1_fc(x) 
         else:
-            output = self.branch2_conv4_x(output)
-            output = self.branch2_conv5_x(output)
-            output = self.avg_pool(output)
-            output = output.view(output.size(0), -1)
-            output = self.branch2_fc(output)
-        return output
+            x = self.branch2_layer3(x)
+            x = self.base_gap(x)
+            x = self.base_flatten(x)
+            x = self.branch2_fc(x)
+        return x
 
     def pred_and_rep(self, x, n):
-        output = self.base_conv1(x)
-        output = self.base_conv2_x(output)
-        output = self.base_conv3_x(output)
-        if n ==0:
-            output = self.avg_pool(output)
-            e = output.view(output.size(0), -1)
-            output = self.branch1_fc(e)
-        else:
-            output = self.branch2_conv4_x(output)
-            output = self.branch2_conv5_x(output)
-            output = self.avg_pool(output)
-            e = output.view(output.size(0), -1)
-            output = self.branch2_fc(e)
-        return output, [e]
-
+        x = self.base_layer0(x)
+        x = self.base_layer1(x)
+        x = self.base_layer2(x)
     
+        if n==0:
+            x = self.base_gap(x)
+            e = self.base_flatten(x)
+            o = self.branch1_fc(e) 
+            return o, [e]
+        else:
+            x2 = self.branch2_layer3(x)
+            x2 = self.base_gap(x2)
+            e2 = self.base_flatten(x2)
+            o2 = self.branch2_fc(e2)
+        return o2, [e2]
+        
+
 class Loss(nn.Module):
     def __init__(self):
         super(Loss, self).__init__()
