32d807b06b59c59f6c1f135fb0933004767c11ce
hoangdzung <hoangdzung@github.com>
2022-06-11 17:41:07 +0200

    add missing method

--- benchmark/xray/model/resnet18_protos.py
+++ benchmark/xray/model/resnet18_protos.py
@@ -130,6 +130,18 @@ class Model(FModule):
         output = self.fc(output)
         return output
 
+    def pred_and_rep(self, x,n=0):
+        output = self.conv1(x)
+        output = self.conv2_x(output)
+        output = self.conv3_x(output)
+        output = self.conv4_x(output)
+        if n>0:
+            output = self.conv5_x(output)
+        output = self.avg_pool(output)
+        e = output.view(output.size(0), -1)
+        output = self.fc(e)
+        return output, e
+
 class Loss(nn.Module):
     def __init__(self):
         super(Loss, self).__init__()
