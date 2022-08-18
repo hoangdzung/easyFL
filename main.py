d9901606c6548a0c3d130d87a5e07d817a07dd7c
hoangdzung <hoangdzung@github.com>
2022-08-18 21:56:22 +1000

    Revert "cal test acc in the best val acc only"
    This reverts commit f705500513eb4ddd1603e260ff5c76b2522bcb57.

--- main.py
+++ main.py
@@ -19,18 +19,18 @@ class MyLogger(flw.Logger):
                 "client_accs":{},
                 "mean_valid_accs":[],
             }
-        # if "mp_" in server.name:
-        #     test_metric, test_loss = server.test(device=torch.device('cuda:0'))
-        # else:
-        #     test_metric, test_loss = server.test()
+        if "mp_" in server.name:
+            test_metric, test_loss = server.test(device=torch.device('cuda:0'))
+        else:
+            test_metric, test_loss = server.test()
         
         valid_metrics, valid_losses = server.test_on_clients(self.current_round, 'valid')
         # train_metrics, train_losses = server.test_on_clients(self.current_round, 'train')
         # self.output['train_losses'].append(1.0*sum([ck * closs for ck, closs in zip(server.client_vols, train_losses)])/server.data_vol)
         self.output['mean_valid_accs'].append(1.0*sum([ck * acc for ck, acc in zip(server.client_vols, valid_metrics)])/server.data_vol)
         self.output['valid_accs'].append(valid_metrics)
-        # self.output['test_accs'].append(test_metric)
-        # self.output['test_losses'].append(test_loss)
+        self.output['test_accs'].append(test_metric)
+        self.output['test_losses'].append(test_loss)
         self.output['mean_curve'].append(np.mean(valid_metrics,0).tolist()) 
         self.output['var_curve'].append(np.std(valid_metrics,0).tolist()) 
         
@@ -38,17 +38,16 @@ class MyLogger(flw.Logger):
             self.output['client_accs'][server.clients[cid].name]=[self.output['valid_accs'][i][cid] for i in range(len(self.output['valid_accs']))]
     
         # print("Training Loss:", self.output['train_losses'][-1])    
-        # print("Testing Loss:", self.output['test_losses'][-1])
-        # print("Testing Accuracy:", self.output['test_accs'][-1]) 
+        print("Testing Loss:", self.output['test_losses'][-1])
+        print("Testing Accuracy:", self.output['test_accs'][-1]) 
         print("Validating Accuracy:", self.output['mean_valid_accs'][-1])
         print("Mean of Client Accuracy:", self.output['mean_curve'][-1])
         print("Std of Client Accuracy:", self.output['var_curve'][-1])
         if self.output['mean_valid_accs'][-1] >= self.best_val_acc:
             self.best_val_acc = self.output['mean_valid_accs'][-1] 
-            server.save_model()
-
-        # print("Overall Testing Accuracy:", self.best_test_acc)
-
+            self.best_test_acc = self.output['test_accs'][-1]
+        print("Overall Testing Accuracy:", self.best_test_acc)
+        
 logger = MyLogger()
 
 def main():
