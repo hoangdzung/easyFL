b046e7c3a02b3f274f7a7737f8ad5e8a719f783d
hoangdzung <hoangdzung@github.com>
2022-08-24 21:17:35 +0700

    test skipnet

---
+++ benchmark/cifar10/model/skipnet_baseline.py
@@ -0,0 +1,61 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from utils.fmodule import FModule
+
+class Model(FModule):
+    def __init__(self):
+        super().__init__()
+        self.layer0 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            # nn.Dropout2d(0.4),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+        )
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+        )
+        
+        self.gap = torch.nn.AdaptiveAvgPool2d(1)
+        self.flatten = nn.Flatten()
+        self.fc = torch.nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.gap(x)
+        x = self.flatten(x)
+        x = self.fc(x)
+
+        return x
+             
+class Loss(nn.Module):
+    def __init__(self):
+        super(Loss, self).__init__()
+        self.cross_entropy = nn.CrossEntropyLoss()
+
+    def forward(self, output, target):
+        return self.cross_entropy(output, target)
